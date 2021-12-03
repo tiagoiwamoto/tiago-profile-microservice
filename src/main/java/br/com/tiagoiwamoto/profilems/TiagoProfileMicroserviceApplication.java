26a7001da41301631a71ec9363a8ade706b1a0ee
Tiago Iwamoto <tiago.iwamoto@gmail.com>
2021-12-03 07:13:08 -0300

    Initial commit

---
+++ src/main/java/br/com/tiagoiwamoto/profilems/TiagoProfileMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagoiwamoto.profilems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiagoProfileMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiagoProfileMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/tiagoiwamoto/profilems/TiagoProfileMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagoiwamoto.profilems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiagoProfileMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
